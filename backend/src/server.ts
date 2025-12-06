492b98e3d297917655b9658b5e0273ad0f3c04c6
Jeffrey M. Aspiras <jefsohandsome1@gmail.com>
2025-12-06 11:06:37 +0800

    initial backend setup commit

---
+++ backend/src/server.ts
@@ -0,0 +1,23 @@
+import Fastify from 'fastify'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+
+const server = Fastify({
+    logger: true
+})
+
+if(!process.env.HTTP_PORT || !process.env.HOST){
+    server.log.error("Missing required environment variables")
+    server.close();
+}
+
+try{
+    server.listen({
+        port: Number(process.env.HTTP_PORT),
+        host: String(process.env.HOST)
+    })
+}catch(err: unknown){
+    server.log.error("An error occured")
+}
